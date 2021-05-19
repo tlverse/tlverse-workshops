d38f0ba3eac47a083d76f67df6a6fc5c346f9a25
Nima Hejazi <nh@nimahejazi.org>
2021-05-19 14:02:28 -0700

    update images

--- img/R/animate_shift.R
+++ img/R/animate_shift.R
@@ -18,44 +18,56 @@ qbar_and_shift <- lapply(delta_grid, function(delta) {
   return(as_tibble(cbind(a_shifted, qbar_aplusdelta)))
 })
 
-saveGIF({
-  for (iter in seq_along(qbar_and_shift)) {
-
-    df_input <- qbar_and_shift[[iter]]
-    a_mean <- mean(df_input$a_shifted)
-    p_a_shifted <- df_input %>%
-      ggplot(aes(x = a_shifted)) +
-        geom_histogram(colour = "white", fill = pal[[5]], alpha = 0.6,
-                       binwidth = 0.2) +
-        geom_vline(xintercept = a_mean, size = 0.5, linetype = "dashed",
-                   colour = "black") +
+saveGIF(
+  {
+    for (iter in seq_along(qbar_and_shift)) {
+      df_input <- qbar_and_shift[[iter]]
+      a_mean <- mean(df_input$a_shifted)
+      p_a_shifted <- df_input %>%
+        ggplot(aes(x = a_shifted)) +
+        geom_histogram(
+          colour = "white", fill = pal[[5]], alpha = 0.6,
+          binwidth = 0.2
+        ) +
+        geom_vline(
+          xintercept = a_mean, size = 0.5, linetype = "dashed",
+          colour = "black"
+        ) +
         xlim(-4, 4) +
         xlab("") +
         ylab("") +
         ggtitle("Exposure profile") +
         theme_bw() +
-        theme(text = element_text(size = 22),
-              axis.text.x = element_text(colour = 'black', size = 22),
-              axis.text.y = element_text(colour = 'black', size = 22))
+        theme(
+          text = element_text(size = 22),
+          axis.text.x = element_text(colour = "black", size = 22),
+          axis.text.y = element_text(colour = "black", size = 22)
+        )
 
-    qbar_mean <- mean(df_input$qbar_aplusdelta)
-    p_qbar_shifted <- df_input %>%
-      ggplot(aes(y = qbar_aplusdelta)) +
-        geom_hline(yintercept = qbar_mean, size = 2, linetype = "dashed",
-                   colour = "black") +
+      qbar_mean <- mean(df_input$qbar_aplusdelta)
+      p_qbar_shifted <- df_input %>%
+        ggplot(aes(y = qbar_aplusdelta)) +
+        geom_hline(
+          yintercept = qbar_mean, size = 2, linetype = "dashed",
+          colour = "black"
+        ) +
         xlab("") +
         ylab("") +
         ylim(0, 1) +
         ggtitle("Counterfactual mean risk") +
         theme_bw() +
-        theme(text = element_text(size = 22),
-              axis.text.x = element_text(colour = 'black', size = 22),
-              axis.text.y = element_text(colour = 'black', size = 22))
+        theme(
+          text = element_text(size = 22),
+          axis.text.x = element_text(colour = "black", size = 22),
+          axis.text.y = element_text(colour = "black", size = 22)
+        )
 
-    p_out <- p_a_shifted + p_qbar_shifted
-    print(p_out)
-  }
-},
-movie.name = here("img", "gif", "shift_animation.gif"), interval = 1,
-ani.width = 1400, ani.height = 1000
+      p_out <- p_a_shifted + p_qbar_shifted
+      print(p_out)
+    }
+  },
+  movie.name = here("img", "gif", "shift_animation.gif"),
+  interval = 1,
+  ani.width = 1400,
+  ani.height = 1000
 )
